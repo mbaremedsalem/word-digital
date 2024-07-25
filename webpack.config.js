5f6eff178068d44eed6c0cca13b50d006f3cc66f
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2024-07-25 19:20:22 +0200

    new commit

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    performance: {
+      hints: 'warning',
+      maxAssetSize: 200000, // Augmenter la limite de taille des assets (en octets)
+      maxEntrypointSize: 400000, // Augmenter la limite de taille des points d'entrée (en octets)
+    },
+  };
+  
